1ecbc3ad55fd7dc8bc061907bc3682ba4532e013
Ignacio Martinez Katzky <nacho.mk@hotmail.com>
2023-06-27 19:33:06 -0300

    ..

---
+++ Clase 6 (js)/practica.js
@@ -0,0 +1,15 @@
+let nombre = 'Bruce Wayne';
+let nombreDeSuperHeroe = 'Batman';
+let vuela = false
+let maneja = true
+let vida = 100
+let trabajo = undefined
+let tiempoLibre = null
+
+console.log('El nombre es --> ' +nombre)
+console.log('El nombre del superheroe es --> ' +nombreDeSuperHeroe)
+console.log('¿El superheroe vuela? --> ' +vuela)
+console.log('¿El superheroe maneja? --> ' +maneja)
+console.log('¿Cuanta vida tiene?  -->  ' +vida)
+console.log('¿De que trabaja? --> ' +trabajo)
+console.log('¿Cuanto tiempo libre tiene? --> ' +tiempoLibre)
