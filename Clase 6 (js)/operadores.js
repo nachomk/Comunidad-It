86b4cf058907c43bc16bce171942256b90655913
Ignacio Martinez Katzky <nacho.mk@hotmail.com>
2023-06-27 21:26:29 -0300

    27/06

---
+++ Clase 6 (js)/operadores.js
@@ -0,0 +1,32 @@
+// let numero1 = 16
+// let numero2 = 10
+
+// let resultadoSuma = numero1 + numero2
+// let resultadoResta = numero1 - numero2
+// let resultadoMultiplicacion = numero1 * numero2
+// let resultadoDivision = numero1 / numero2
+// let resultadoResto = numero1 % numero2
+
+// console.log('El resultado de la suma es igual a: ' + resultadoSuma)
+// console.log('El resultado de la resta es igual a: ' + resultadoResta)
+// console.log('El resultado de la multiplicaciòn es igual a: ' + resultadoMultiplicacion)
+// console.log('El resultado de la divisiòn es igual a: ' + resultadoDivision)
+// console.log('El resultado del resto es igual a: ' + resultadoResto)
+
+// ${esto sirve para dentro de un string, agregar una variable con las contracomillas --> (``)} 
+// === --> compara tipo de dato
+// == compara la simbologìa
+
+// let prueba1 = true;
+// let prueba2 = false;
+
+// let resultadoAnd = true && false;
+// let resultadoOr = true || false;
+
+let promedioParaAprobar = 6;
+let promedioDelAlumno = 4;
+let promedio = promedioDelAlumno >= promedioParaAprobar;
+let puntosFaltantes = promedioParaAprobar - promedioDelAlumno;
+
+console.log(`¿El alumno tiene un promedio mayor o igual que el requerido? ` +promedio)
+console.log(`Al alumno le faltaron ${puntosFaltantes} puntos para aprobar`)
