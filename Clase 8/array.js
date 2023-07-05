80952a13e03b50b2a8d11a98b8f55350d7a8a085
Ignacio Martinez Katzky <nacho.mk@hotmail.com>
2023-07-04 21:23:42 -0300

    04/07

---
+++ Clase 8/array.js
@@ -0,0 +1,37 @@
+// 1)
+// let suma = 0;
+// let arrayNumeros = [1,2,3,4,5];
+
+// for (let itemArray of arrayNumeros ){
+//     console.log(itemArray)
+//     suma = suma + itemArray
+// }
+
+// console.log(`El resultado es igual a ${suma}`)
+
+// 2)
+// let numeros = [1, 50, 8, 2, 3];
+// let maximo = 16;
+
+// for (let item of numeros) {
+//     if (maximo < item) {
+//         console.log(`El numero ${maximo} es menor que ${item}`);
+//         maximo = item;
+//     }
+// }
+// console.log(maximo);
+
+// // 3)
+// let arrayNumeros = [1, 2, 3, 4, 5, 6];
+// let arrayPares = [];
+// let arrayImpares = [];
+
+// for (let item of arrayNumeros) {
+//     if (item % 2 === 0) {
+//         arrayPares.push(item);
+//     } else {
+//         arrayImpares.push(item)
+//     }
+// }
+// console.log(arrayPares);
+// console.log(arrayImpares);
---
+++ Clase 8/funciones.js
@@ -0,0 +1,14 @@
+const calculadora = function  (num1, num2, operacion) {
+    if (operacion === '+') {
+        console.log(num1 + num2);
+    }
+    if (operacion === '-'){
+        console.log(num1 - num2)
+    }
+    if (operacion === '*'){
+        console.log(num1 * num2)
+    }
+    if (operacion === '/'){
+        console.log(num1 / num2)
+    }
+}
