588864ce5b4162fcfe1707768fe67610bda67ece
Nacho <nacho.mk@hotmail.com>
2023-07-07 11:20:53 -0300

    calculadora

--- Clase 9/calculadora.js
+++ Clase 9/calculadora.js
@@ -1,14 +1,14 @@
 let calculadora = {
     sumar: function (num1,num2) {
-        console.log(`La suma de ambos números es igual a ${this.numero_1 + this.numero_2}`)
+        console.log(`La suma de ambos números es igual a ${num1 + num2}`)
     },
     restar: function (num1,num2) {
-        console.log(`La diferencia de ambos números es igual a ${this.numero_1 - this.numero_2}`)
+        console.log(`La diferencia de ambos números es igual a ${num1 - num2}`)
     },
     multiplicacion: function (num1,num2) {
-        console.log(`El producto de ambos números es igual a ${this.numero_1 * this.numero_2}`)
+        console.log(`El producto de ambos números es igual a ${num1 * num2}`)
     },
     division: function (num1,num2) {
-        console.log(`La división de ambos números es igual a ${this.numero_1 + this.numero_2}`)
+        console.log(`La división de ambos números es igual a ${num1 + num2}`)
     }
 }
