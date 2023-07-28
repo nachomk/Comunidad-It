af673558f1078b1aa0b3bb1624f662b1bc2c83d3
Ignacio Martinez Katzky <nacho.mk@hotmail.com>
2023-07-27 21:26:34 -0300

    27/07

--- Clase 14 (back)/clase.js
+++ Clase 14 (back)/clase.js
@@ -6,6 +6,10 @@
 // })
 
 const listaUsuarios = document.getElementById('listaUsuarios');
+const listaTitulos = document.getElementById('listaTitulos');
+const listaId = document.getElementById('listaId');
+const listaFoto = document.getElementById('listaFoto');
+
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
@@ -21,17 +25,57 @@ fetch('https://jsonplaceholder.typicode.com/users')
         elementoListaBoton.addEventListener ('click', (e) => {
         e.preventDefault();
         mostrarInfoDeUsuario(data.id);
-        })
+       
+
+            })
         })
     })
-    
 
-
-function mostrarInfoDeUsuario(id) {
-    fetch(`https://jsonplaceholder.typicode.com/albums?userId=${id}`)
+function mostrarInfoDeUsuario(idAlbum) {
+    fetch(`https://jsonplaceholder.typicode.com/albums?userId=${idAlbum}`)
     .then(response => response.json())
-    .then((datos) => {
-        console.log(datos)
+    .then((datosAlbum) => {
+        // console.log(datosAlbum)
+        listaTitulos.innerHTML = '';
+        listaId.innerHTML = '';
+        datosAlbum.forEach((dato) => {
+            let elementoListaTitulo = document.createElement('li')
+            let elementoListaTituloBoton = document.createElement('button')
+            elementoListaTituloBoton.innerText = `${dato.title}`
+            listaTitulos.appendChild(elementoListaTitulo)
+            elementoListaTitulo.appendChild(elementoListaTituloBoton)
+
+            let elementoListaId = document.createElement('li')
+            elementoListaId.innerText = `${dato.id}`
+            listaId.appendChild(elementoListaId)
+
+
+
+            elementoListaTituloBoton.addEventListener('click', () => {
+
+                mostrarFotoTitulo(dato.id)
+
+
+            })
+        })
     })
 }
 
+
+function mostrarFotoTitulo(id) {
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
+    .then(response => response.json())
+    .then((datoFotos) => {
+        listaFoto.innerHTML= '';
+        datoFotos.forEach ((datoFoto) => {
+            let elementoListaFoto = document.createElement('li')
+            elementoListaFoto.innerText = `${datoFoto.title}`
+            listaFoto.appendChild(elementoListaFoto)
+
+            let elementoFoto = document.createElement('img')
+            elementoFoto.setAttribute('src',datoFoto.url)
+            listaFoto.appendChild(elementoFoto)
+
+        })
+    })
+}
