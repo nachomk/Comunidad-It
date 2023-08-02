1a67e5894e7b326a85d6dbde3f24c45c45cd6641
Ignacio Martinez Katzky <nacho.mk@hotmail.com>
2023-08-01 21:36:13 -0300

    01/08

--- Clase 14 (back)/clase.js
+++ Clase 14 (back)/clase.js
@@ -11,34 +11,39 @@ const listaId = document.getElementById('listaId');
 const listaFoto = document.getElementById('listaFoto');
 
 
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then((datos) => {
-        //console.log(datos);
-        datos.forEach((data) => {
-        let elementoLista = document.createElement('li');
-        let elementoListaBoton = document.createElement('button')
-        elementoListaBoton.innerText = `${data.name}`
-        listaUsuarios.appendChild(elementoLista);
-        elementoLista.appendChild(elementoListaBoton);
-        
-        elementoListaBoton.addEventListener ('click', (e) => {
-        e.preventDefault();
-        mostrarInfoDeUsuario(data.id);
-       
 
+async function obtenerUsuarios() {
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/users');
+        let usuarios = await response.json();
+        usuarios.forEach((data) => {
+            let elementoLista = document.createElement('li');
+            let elementoListaBoton = document.createElement('button')
+            elementoListaBoton.innerText = `${data.name}`
+            listaUsuarios.appendChild(elementoLista);
+            elementoLista.appendChild(elementoListaBoton);
+            
+            elementoListaBoton.addEventListener ('click', (e) => {
+            e.preventDefault();
+            mostrarInfoDeUsuario(data.id);
+                })
             })
-        })
-    })
+    } catch(error) {
+        console.log(error)
+    } finally {
 
-function mostrarInfoDeUsuario(idAlbum) {
-    fetch(`https://jsonplaceholder.typicode.com/albums?userId=${idAlbum}`)
-    .then(response => response.json())
-    .then((datosAlbum) => {
-        // console.log(datosAlbum)
+    }
+}
+obtenerUsuarios()
+
+
+async function mostrarInfoDeUsuario (idAlbum) {
+    try {
+        let response = await fetch(`https://jsonplaceholder.typicode.com/albums?userId=${idAlbum}`)
+        let infoUsuarios = await response.json();
         listaTitulos.innerHTML = '';
         listaId.innerHTML = '';
-        datosAlbum.forEach((dato) => {
+        infoUsuarios.forEach((dato) => {
             let elementoListaTitulo = document.createElement('li')
             let elementoListaTituloBoton = document.createElement('button')
             elementoListaTituloBoton.innerText = `${dato.title}`
@@ -49,33 +54,55 @@ function mostrarInfoDeUsuario(idAlbum) {
             elementoListaId.innerText = `${dato.id}`
             listaId.appendChild(elementoListaId)
 
-
-
             elementoListaTituloBoton.addEventListener('click', () => {
-
                 mostrarFotoTitulo(dato.id)
-
-
             })
         })
-    })
+    } catch(error) {
+        console.log(error) 
+    } finally {
+
+    }
 }
+mostrarInfoDeUsuario()
+
+async function mostrarFotoTitulo (id) {
+    try {
+        let response = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
+    let fotoTitulo = await response.json();
+    listaFoto.innerHTML= '';
+    fotoTitulo.forEach ((datoFoto) => {
+        let elementoListaFoto = document.createElement('li')
+        elementoListaFoto.innerText = `${datoFoto.title}`
+        listaFoto.appendChild(elementoListaFoto)
+
+        let elementoFoto = document.createElement('img')
+        elementoFoto.setAttribute('src',datoFoto.url)
+        listaFoto.appendChild(elementoFoto)
+    })
+    } catch(error) {
+        console.log(error)
+    } finally {
+
+    }
+}
+mostrarFotoTitulo()
 
 
-function mostrarFotoTitulo(id) {
-    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
-    .then(response => response.json())
-    .then((datoFotos) => {
-        listaFoto.innerHTML= '';
-        datoFotos.forEach ((datoFoto) => {
-            let elementoListaFoto = document.createElement('li')
-            elementoListaFoto.innerText = `${datoFoto.title}`
-            listaFoto.appendChild(elementoListaFoto)
-
-            let elementoFoto = document.createElement('img')
-            elementoFoto.setAttribute('src',datoFoto.url)
-            listaFoto.appendChild(elementoFoto)
-
+async function mostrarDatos() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts',
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
         })
-    })
+    });
+    const result = await response.json();
+    console.log(result)
 }
+mostrarDatos()
