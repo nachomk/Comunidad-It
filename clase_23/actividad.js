40f6a615f7b4236fdb97ca324498fb1e8bd3e20a
Ignacio Martinez Katzky <nacho.mk@hotmail.com>
2023-08-31 21:14:57 -0300

    31/08

--- clase_23/actividad.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require("express");
-
-const Products = require("./schemas/UserSchema.js")
-const { conectarDB , desconectarDB } = require("./connection");
-const PORT = process.env.PORT || 8000;
-const app = express();
-
-app.use(express.json());
